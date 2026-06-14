58bd2a9e2cb189587bc519d4622c1b897540c07c
ShubhamMurshetwar <shubhammurshetwar123@gmail.com>
2026-06-15 03:23:25 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -60,7 +60,9 @@ async function handleSignIn(event) {
   }
 
   try {
-    const res = await fetch("http://127.0.0.1:5000/login", {
+    const API_URL = "https://fake-profile-detection-s2md.onrender.com";
+
+fetch(`${API_URL}/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -115,7 +117,7 @@ async function handleSignUp(event) {
   }
 
   try {
-    const res = await fetch("http://127.0.0.1:5000/signup", {
+    const res = await fetch(`${API_URL}/signup`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
